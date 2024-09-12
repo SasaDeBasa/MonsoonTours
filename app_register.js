7271dac9715fc748947442be623eee7a66bae250
SasaDeBasa <sasaekan1234@gmail.com>
2024-09-12 19:55:38 +0530

    authentication init

--- app_register.js
+++ app_register.js
@@ -1,17 +1,16 @@
-// Import Firebase modules
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
+ // Firebase Initialization
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+ import { getFirestore, doc, setDoc, collection, getDocs, deleteDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_FIREBASE_AUTH_DOMAIN",
-  projectId: "YOUR_FIREBASE_PROJECT_ID",
-  storageBucket: "YOUR_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "YOUR_FIREBASE_APP_ID",
-  measurementId: "YOUR_FIREBASE_MEASUREMENT_ID",
-};
+ const firebaseConfig = {
+     apiKey: "AIzaSyCOm3GlA2_UgZhhHD_zDU9BRFwLnOLueEA",
+     authDomain: "monsoontours-65f1e.firebaseapp.com",
+     projectId: "monsoontours-65f1e",
+     storageBucket: "monsoontours-65f1e.appspot.com",
+     messagingSenderId: "378330088807",
+     appId: "1:378330088807:web:217c00702fc17fea671bc2",
+     measurementId: "G-L4V5MLH9KD"
+ };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
